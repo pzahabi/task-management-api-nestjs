65111ecb0a4f76f788d64c2fae9b2df15b2f1959
Pooya Zahabi <zahabipooya@gmail.com>
2024-09-23 13:12:33 +0330

    env config added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,15 +7,21 @@ import UserRepository from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import JwtStrategy from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt'}),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        expiresIn: 3600
-      }
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600
+        }
+      })
     }),
     TypeOrmModule.forFeature([User])
   ],
